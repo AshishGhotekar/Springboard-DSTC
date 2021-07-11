c3c64fc494114f4f9e6f153972ba5e194bc0e571
Ashish Ghotekar <ashishghotekar@gmail.com>
2021-07-11 14:15:40 -0500

    Update Unit02_interestRate.py

--- Unit02_interestRate.py
+++ Unit02_interestRate.py
@@ -6,22 +6,22 @@ Created on Fri Jul  9 16:42:31 2021
 @author: aghotekar
 """
 
-''' 15% interest rate '''
+# 15% interest rate
 interest_rate = 1.15
 
-''' initial principal amount '''
+# initial principal amount
 money = 1000
 
-''' time of investment in years'''
+# time of investment in years
 term = 5
 
-''' money spent after five years '''
+# money spent after five years
 lump_sum = 500
 
-''' amonut left after five years '''
+# amonut left after five years
 after_five_years = (money * (interest_rate ** term)) - lump_sum
 print(after_five_years)
 
-''' amount left after ten years '''
+# amount left after ten years
 after_ten_years = (after_five_years * (interest_rate ** term))
 print(after_ten_years)
